a67939aedb12a28662a2bf39e56b2bd467d8e978
unknown <jh.2220@hotmail.com>
2022-10-17 14:45:11 -0300

    count roll

--- server.js
+++ server.js
@@ -7,13 +7,24 @@ const path = require('path')
 app.use(express.static('public'))
 app.use(express.static('files'))
 
+let count = 0;
+
 const mascotsFiles = ['clippy', 'rover', 'merlin','bonzi','links','genie']
 
 app.get('/', (req, res) => {
 
-    let randomMascot = mascotsFiles[Math.floor(Math.random() * mascotsFiles.length)]
+    //let randomMascot = mascotsFiles[Math.floor(Math.random() * mascotsFiles.length)]
 
-    res.sendFile(path.join(__dirname, `mascots/${randomMascot}.png`))
+  console.log(count, mascotsFiles.length);
+
+  count++;
+
+  if(count > (mascotsFiles.length - 1)){
+    count = 0;
+  }
+
+  res.sendFile(path.join(__dirname, `mascots/${mascotsFiles[count]}.png`))
+
 })
 
 app.listen(process.env.PORT || 3000, () => {
